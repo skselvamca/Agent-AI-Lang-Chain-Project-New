937aa2e4ed8a289a923a6c89cade413292f6fbee
S Selvakumar <skselvamca3@gmail.com>
2026-04-10 00:36:00 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
-from duckduckgo_search import DDGS
+from langchain.tools import Tool
+from langchain_community.tools.tavily_search import TavilySearchResults
 
-def search_tool(query):
-    with DDGS() as ddgs:
-        results = ddgs.text(query, max_results=5)
-        return [r["body"] for r in results]
+search = TavilySearchResults()
+
+search_tool = Tool(
+    name="Search",
+    func=search.run,
+    description="Use this for latest news and real-time information"
+)
